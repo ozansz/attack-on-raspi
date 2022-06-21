a8229bee8c83a81da3e9e684ddb562ecdc277b50
Ozan Sazak <ozan.sazak@protonmail.ch>
2022-06-21 16:24:54 +0300

    update 01 files

--- scripts/01-syn-flood/client.py
+++ scripts/01-syn-flood/client.py
@@ -4,6 +4,6 @@ import requests
 import config
 
 while True:
-    r = requests.get(f"http://localhost:{config.SERVER_PORT}")
+    r = requests.get(f"http://{config.SERVER_IP}:{config.SERVER_PORT}")
     print(r.status_code)
     time.sleep(config.SLEEP_BETWEEN_REQUESTS)
